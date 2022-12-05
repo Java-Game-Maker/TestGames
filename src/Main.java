822b942930d6d7dac9c3444940c2fa6b77b7ebb1
spy <alfred@stensatter.se>
2022-12-05 22:20:25 +0100

    update

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+import com.javagamemaker.javagameengine.JavaGameEngine;
+import com.javagamemaker.javagameengine.Scene;
+import com.javagamemaker.javagameengine.components.*;
+import com.javagamemaker.javagameengine.msc.Vector2;
+
+
+public class Main extends JavaGameEngine {
+
+    public static void main(String[] args) {
+        Scene scene = new Scene();
+
+        for(int i = 0; i < 100;i++){
+            GameObject gameObject = new GameObject();
+            gameObject.add(new PhysicsBody(true));
+            gameObject.add(new Collider());
+            gameObject.setPosition(new Vector2((i)*100-500,i*100));
+            gameObject.updateVertices();
+            scene.add(gameObject);
+        }
+        setSelectedScene(scene);
+
+        Scene scene1 = new Scene();
+        GameObject parent = new GameObject();
+        for(int i = 0; i < 100;i++){
+            GameObject gameObject = new GameObject();
+            gameObject.add(new PhysicsBody(true));
+            //gameObject.add(new Collider());
+            //gameObject.setParentOffset(new Vector2(i*100+50,1));
+            parent.add(gameObject);
+        }
+        scene1.add(parent);
+        setSelectedScene(scene);
+
+        start();
+    }
+
+}
